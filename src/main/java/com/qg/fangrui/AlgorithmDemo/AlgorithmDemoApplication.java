b3afca2845033788b193b2f5a3c858e9ba5e81fb
FunriLy <fr479910770@qq.com>
2017-08-09 09:03:11 +0800

    init

---
+++ src/main/java/com/qg/fangrui/AlgorithmDemo/AlgorithmDemoApplication.java
@@ -0,0 +1,12 @@
+package com.qg.fangrui.AlgorithmDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmDemoApplication.class, args);
+	}
+}
